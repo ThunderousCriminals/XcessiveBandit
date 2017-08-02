e8a9d7deba8cf013241fae96451a4157c71bd189
Ash <eshwarenm@gmail.com>
2017-08-02 21:02:19 +0800

    Fixed responsive stuff

---
+++ scripts.js
@@ -0,0 +1,9 @@
+//Array containing all the GIFs
+
+
+
+//Get a random number in range between zero and GIF array's length
+
+
+
+//Set the body element's background image using the random number generated above
