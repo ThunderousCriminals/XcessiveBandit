5e8738335776cfc9869a74242301306f33e41975
Ash <eshwarenm@gmail.com>
2017-08-02 23:49:55 +0800

    Cleanup..

--- scripts.js
+++ scripts.js
@@ -1,15 +1,8 @@
-//Array containing all the GIFs
-    //Initialize array with all gifs
-    let gifArray = ['1.gif','2.gif','3.gif','4.gif','5.gif','6.gif','7.gif','8.gif','9.gif','10.gif']
+//Initialize array with all gifs
+const gifs = ['1.gif', '2.gif', '3.gif', '4.gif', '5.gif', '6.gif', '7.gif', '8.gif', '9.gif', '10.gif'];
 
-//Get a random number in range between zero and GIF array's length
-    //select random index between 0 - the final index of the array.
-    let randIndex = Math.floor(Math.random()*gifArray.length)
-   
-    
-//Set the body element's background image using the random number generated above
-    //get selected gif.
-    let randGif = gifArray[randIndex]
-    //set the background image of body element to the randomly selected gif.
-    let changeBG = document.body
-    changeBG.style.backgroundImage = "url("+ randGif+")"
+//Select random number between 0 - length of the array.
+const rand = Math.floor(Math.random() * gifs.length);
+
+//Change the body element's background-image
+document.body.style.backgroundImage = 'url(' + gifs[rand] + ')';
